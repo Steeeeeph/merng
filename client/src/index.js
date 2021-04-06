a09f0f42d85bd2a90054752b126e60dc0f1d5be1
Stephanie Van Bockhaven <stephanie.vanbockhaven@hotmail.com>
2021-04-06 15:45:19 +0200

    crud operations with db are working

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,22 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloLink } from "apollo-link";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from 'apollo-link-error'
+import Notifications, {notify} from 'react-notify-toast';
 import dotenv from 'dotenv';
 // import './index.css';
 import App from './App';
 // import * as serviceWorker from "./serviceWorker";
 
-dotenv.config({path: '/server/.env'});
+dotenv.config();
 
-const httpLink = createHttpLink({ uri: `http://localhost:${process.env.PORT_SERVER}graphql` });
-const link = ApolloLink.from([httpLink]);
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message }) => notify.show(message, 'error'))
+})
+// TODO fix the .env client side
+const httpLink = createHttpLink({ uri: 'http://localhost:4300/graphql' });
+// const httpLink = createHttpLink({ uri: `http://localhost:${process.env.PORT_SERVER}/graphql` });
+const link = ApolloLink.from([errorLink, httpLink]);
 
 const client = new ApolloClient({
   link,
@@ -22,6 +29,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <Notifications />
     <App />
   </ApolloProvider>,
   document.getElementById("root")
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 import {graphqlHTTP} from 'express-graphql';
@@ -6,11 +7,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(cors());
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(process.env.PORT_SERVER, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT_SERVER}`)))
 .catch((error) => console.log(`${error} did not connect`));
+
 
 app.get("/", (req, res) => {
     res.json({
